27dbae9e1359deb66ce1bb14fda5aa88f53639cd
Shamansk1y <shamansky1225@gmail.com>
2023-03-03 20:22:38 +0100

    done

--- d_site/settings.py
+++ d_site/settings.py
@@ -31,7 +31,7 @@ DB_PORT = os.environ.get('DB_PORT')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kider-site.herokuapp.com']
 
